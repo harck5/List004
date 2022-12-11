3aab5ae034fc16196f1bb9b226e9088af51cd1eb
harck5 <rauljimenezhk@gmail.com>
2022-12-11 13:06:20 +0100

    cosas

---
+++ Assets/Scripts/Exercise001.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exercise001 : MonoBehaviour
+{
+    
+    void Update()
+    {
+        
+    }
+    private void Caja(int Money)
+    {
+        
+    }
+}
