c417e40a061edc5611c70b2feec7e777cf0fd8b9
harck5 <rauljimenezhk@gmail.com>
2023-04-19 11:27:22 +0200

    fcgbd

--- Assets/Scripts/Exercise005.cs
+++ Assets/Scripts/Exercise005.cs
@@ -22,7 +22,7 @@ public class Exercise005 : MonoBehaviour
             Debug.Log("The object it's out of bounds");
         }
     }
-    private bool OutOfBounds(float limitX, float limitY, float limitZ)//comprovacion de los numeros absolutos que vayamo a escrivir en las variables
+    private bool OutOfBounds(float limitX, float limitY, float limitZ)//comprobacion de los numeros absolutos que vayamo a escribir en las variables
     {
         if (transform.position.x <= limitX && transform.position.y <= limitY && transform.position.z <= limitZ && transform.position.x >= (-1 * limitX) && transform.position.y >= (-1 * limitY) && transform.position.z >= (-1 * limitZ))
         {
